19e63a79559ddb239f7367392a09bb30aff8d9ab
Santiago Gabriel Vollmar <santiago.vollmar@gmail.com>
2018-10-30 10:52:20 +0100

    Improved Property reader utility class

--- src/api-prototype/src/main/java/api/config/PropertyReader.java
+++ src/api-prototype/src/main/java/api/config/PropertyReader.java
@@ -1,6 +1,7 @@
 package api.config;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Properties;
 
 /**
@@ -10,16 +11,34 @@ import java.util.Properties;
  * @author Santiago Gabriel Vollmar
  */
 public class PropertyReader {
-
+	
+	private static final HashMap<String, Properties> readFileProperties = new HashMap<String, Properties>();
+	
+	private static Properties getCachedProperties(String fileURL) {
+		synchronized(readFileProperties) {
+			return readFileProperties.get(fileURL);
+		}
+	}
+	
+	private static void cacheProperties(String fileURL, Properties properties) {
+		synchronized(readFileProperties) {
+			readFileProperties.put(fileURL, properties);
+		}
+	} 
+	
 	private Properties properties;
 
 	/**
 	 * @param fileURL
 	 */
 	public PropertyReader(String fileURL) throws IOException {
-
-		properties = new Properties();
-		properties.load(getClass().getClassLoader().getResourceAsStream(fileURL));
+		properties = getCachedProperties(fileURL);
+		
+		if (properties == null) {
+			properties = new Properties();
+			properties.load(getClass().getClassLoader().getResourceAsStream(fileURL));
+			cacheProperties(fileURL, properties);
+		}
 	}
 
 	/**
@@ -30,12 +49,13 @@ public class PropertyReader {
 	 *
 	 * @return Requested property as an integer
 	 */
-	public int getIntProperty(String propName) {
-
+	public Integer getIntProperty(String propName) {
 		String propVal = properties.getProperty(propName);
-		int intPropVal = Integer.parseInt(propVal);
-		return intPropVal;
-
+		if (propVal != null) {
+			Integer intPropVal = Integer.parseInt(propVal);
+			return intPropVal;
+		}
+		return null; 
 	}
 
 	/**
@@ -46,10 +66,8 @@ public class PropertyReader {
 	 * @return Requested property as a string
 	 */
 	public String getStringProperty(String propName) {
-
 		String propVal = properties.getProperty(propName);
 		return propVal;
-
 	}
 
 }
